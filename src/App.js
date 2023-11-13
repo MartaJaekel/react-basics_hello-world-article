8e704f81262fa0419d450e7ec78c7ad1c65a873d
Marta Jäkel <martajaekel@gmail.com>
2023-11-13 12:45:20 +0100

    feat: HelloWorldArticle

--- src/App.js
+++ src/App.js
@@ -2,5 +2,21 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <div>Replace me with your Component!</div>;
+  return(
+    <div>
+      < HelloWorldArticle/>
+    </div>
+
+  );
+}
+
+function HelloWorldArticle() {
+  return  <div>
+ 
+  <article>
+    <h1>Welcome!</h1>
+    <p>currently learning react</p>
+
+  </article>
+  </div>
 }
